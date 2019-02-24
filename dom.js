6ca5a918155f212a896487ac10fa7e3600dd4ea3
Igor Oliinyk <igor.oleynikdp@gmail.com>
2019-02-24 13:50:04 +0200

    Завернули dom в обертку Добавили APP.js в документ

--- dom.js
+++ dom.js
@@ -1,20 +1,24 @@
-function addUserToDom(name) {
-  const node = document.createElement("li");
-  const text = document.createTextNode(name);
-  node.appendChild(text);
+function domWrapper() {
+  function addUserToDom(name) {
+    const node = document.createElement("li");
+    const text = document.createTextNode(name);
+    node.appendChild(text);
 
-  document.getElementById("users").appendChild(node);
+    document.getElementById("users").appendChild(node);
+  }
+
+  document.getElementById("submit").addEventListener("click", function() {
+    var input = document.getElementById("input");
+    addUserToDom(input.value);
+
+    input.value = "";
+  });
+
+  var users = APP.getUsers();
+
+  for (var i = 0; i < users.length; i++) {
+    addUserToDom(users[i]);
+  }
 }
 
-document.getElementById("submit").addEventListener("click", function() {
-  var input = document.getElementById("input");
-  addUserToDom(input.value);
-
-  input.value = "";
-});
-
-var users = window.getUsers();
-
-for (var i = 0; i < users.length; i++) {
-  addUserToDom(users[i]);
-}
+domWrapper();
